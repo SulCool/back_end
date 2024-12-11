43a5616a5e33b5361e77a838583f48e90a065f93
unknown <quasare>
2024-12-11 23:56:48 +0300

    shablon

--- bd.js
+++ bd.js
@@ -6,26 +6,42 @@ const db = new sqlite3.Database("./main.db", sqlite3.OPEN_READWRITE, (err) => {
     if(err) return console.error(err.message);
 });
 
-//update
 
+//update
 // sql = "UPDATE users SET name = ? WHERE id = ? ";
 // db.run(sql,["Jake",1], (err) =>{
 //     if(err) return console.error(err.message);
 // })
 
 //create tabl
-// sql = "CREATE TABLE users(id INTEGER PRIMARY KEY, login, Surname, Name, Patronymic, status, roule, post, avg_time)";
+// sql = "CREATE TABLE users(id INTEGER PRIMARY KEY, Login, Name, Surname, Patronymic,Role,Post)";
 // db.run(sql);
-//drop 
+
+//drop table
+// sql = "DROP TABLE users";
+// db.run(sql);
 
 //insert
-// sql = "INSERT INTO users(login, Surname, Name, Patronymic, status, roule, post, avg_time) VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
-// db.run(sql, ["admin", "admin", "admin", "admin", "admin", "admin", "admin", "admin"],
+// sql = "INSERT INTO users(Login, Surname, Name, Patronymic, Role, Post) VALUES(?, ?, ?, ?, ?, ?)";
+// db.run(sql, ["PGiZ5", "Progrev", "Iz", "Goev", "Admin", "Director"],
 //     (err) => {
 //         if(err) return console.error(err.message);
 //     }
 // );
 
+//update
+// sql = "UPDATE users SET name = ? WHERE id = ?";
+// db.run(sql,[1],(err)=>{
+//     if(err) return console.error(err.message);
+// });
+
+// sql = "DELETE FROM users WHERE id = ?";
+// db.run(sql,[1],(err)=>{
+//     if(err) return console.error(err.message);
+// });
+
+
+//select
 // sql = "SELECT * FROM users";
 // db.all(sql,[],(err,rows) => {
 //     if (err) return console.error(err.message);

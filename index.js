2596c9bff5497a50c7c20de2a1903dcb0bbe9f12
unknown <quasare>
2024-12-13 04:22:30 +0300

    ref

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 import express from "express";
-import { add_user } from "./bd.js";
-import { get_users } from "./bd.js";
+import { get_users,update_user,delete_user,add_user } from "./bd.js";
 import cookieParser from "cookie-parser";
-import { delete_user } from "./bd.js";
-import { update_user } from "./bd.js";
 
-const app = express();
+export const app = express();
 const PORT = 3000;
-const __dirname = import.meta.dirname;
-const urlencodedParser = express.urlencoded({ extended: false });
+export const __dirname = import.meta.dirname;
+export const urlencodedParser = express.urlencoded({ extended: false });
 
 
 app.use(cookieParser());
@@ -37,7 +34,6 @@ app.use((req, res, next) => {
     });
 });
 
-
 app.get("/", (req, res) => {
     const tasks = [{ title: "title", description: "description", deadline: "deadline", date: "date" }];
     const users = [{ surname: "surname", name: "name", patronymic: "patronymic" }];
@@ -45,6 +41,33 @@ app.get("/", (req, res) => {
     console.log("Текущий пользователь:", currentUser);
     res.render("main", { tasks, users, currentUser });
 });
+
+app.get("/profile", (req, res) => {
+    const currentUser = res.locals.user;
+    if (!currentUser) {
+        return res.redirect("/log_sign");
+    }
+    res.render("profile", { currentUser });
+});
+
+app.get("/log_sign", (_, res) => {
+    res.render("log_sign", { error: null });
+});
+app.post("/log_sign", urlencodedParser, (req, res) => {
+    if (!req.body || !req.body.login || req.body.login.trim() === "") {
+        return res.render('log_sign', { error: "Введите логин" });
+    }
+    res.clearCookie("login");
+    const query = 'SELECT * FROM users WHERE login = ?';
+    get_users(query, [req.body.login], (err, rows) => {
+        if (err || rows.length === 0) {
+            return res.render('log_sign', { error: "Неправильный логин" });
+        }
+        res.cookie("login", req.body.login, { httpOnly: true });
+        res.redirect("/");
+    });
+});
+
 
 app.get("/edits", (req, res) => {
     const login = req.cookies.login;
@@ -66,6 +89,7 @@ app.get("/edits", (req, res) => {
         }
     });
 });
+
 
 app.post("/edits", urlencodedParser, (req, res) => {
     const { old_login, login, surname, name, patronymic, post, role } = req.body;
@@ -124,73 +148,9 @@ app.post("/edits", urlencodedParser, (req, res) => {
     });
 });
 
-
-
-app.get("/add", (_, res) => {
-    res.sendFile(__dirname + "/views/add.html");
-});
-
-app.post("/add", urlencodedParser, function (req, res) {
-    const { login, surname, name, patronymic, post, role } = req.body;
-
-    if (!login || !surname || !name || !patronymic || !post) {
-        return res.render('add', { error: 'Все поля должны быть заполнены' });
-    }
-
-    const query = 'SELECT * FROM users WHERE login = ?';
-    get_users(query, [login], (err, row) => {
-        if (err) {
-            console.error('Ошибка при выполнении запроса:', err.message);
-            return res.render('add', { error: 'Ошибка сервера' });
-        }
-
-        if (row.length > 0) {
-            return res.render('add', { error: 'Логин уже занят' });
-        }
-
-        const userRole = role === "on" ? "Admin" : "User";
-        add_user(name, surname, patronymic, login, userRole, post);
-        res.redirect(`/`);
-    });
-});
-
-app.post("/add_task", urlencodedParser, function (req, res) {
-    if (!req.body) return res.sendStatus(400);
-    console.log(res.body);
-    res.send(`${req.body.title} - ${req.body.description} - ${req.body.date} - ${req.body.deadline} - ${req.body.name}`);
-});
-
-app.get("/profile", (req, res) => {
-    const currentUser = res.locals.user;
-    if (!currentUser) {
-        return res.redirect("/log_sign");
-    }
-    res.render("profile", { currentUser });
-});
-
-app.get("/log_sign", (_, res) => {
-    res.render("log_sign", { error: null });
-});
-app.post("/log_sign", urlencodedParser, (req, res) => {
-    if (!req.body || !req.body.login || req.body.login.trim() === "") {
-        return res.render('log_sign', { error: "Введите логин" });
-    }
-    res.clearCookie("login");
-    const query = 'SELECT * FROM users WHERE login = ?';
-    get_users(query, [req.body.login], (err, rows) => {
-        if (err || rows.length === 0) {
-            return res.render('log_sign', { error: "Неправильный логин" });
-        }
-        res.cookie("login", req.body.login, { httpOnly: true });
-        res.redirect("/");
-    });
-});
-
-
 app.get("/del", (req, res) => {
     res.render("del");
 });
-
 app.post("/del", urlencodedParser, (req, res) => {
     const { login } = req.body;
 
@@ -229,9 +189,44 @@ app.post("/del", urlencodedParser, (req, res) => {
         });
     });
 });
+
+app.post("/add_task", urlencodedParser, function (req, res) {
+    if (!req.body) return res.sendStatus(400);
+    console.log(res.body);
+    res.send(`${req.body.title} - ${req.body.description} - ${req.body.date} - ${req.body.deadline} - ${req.body.name}`);
+});
+
+app.get("/add", (_, res) => {
+    res.sendFile(__dirname + "/views/add.html");
+});
+app.post("/add", urlencodedParser, function (req, res) {
+    const { login, surname, name, patronymic, post, role } = req.body;
+
+    if (!login || !surname || !name || !patronymic || !post) {
+        return res.render('add', { error: 'Все поля должны быть заполнены' });
+    }
+
+    const query = 'SELECT * FROM users WHERE login = ?';
+    get_users(query, [login], (err, row) => {
+        if (err) {
+            console.error('Ошибка при выполнении запроса:', err.message);
+            return res.render('add', { error: 'Ошибка сервера' });
+        }
+
+        if (row.length > 0) {
+            return res.render('add', { error: 'Логин уже занят' });
+        }
+
+        const userRole = role === "on" ? "Admin" : "User";
+        add_user(name, surname, patronymic, login, userRole, post);
+        res.redirect(`/`);
+    });
+});
+
+
 app.get("/logout", (req, res) => {
-    res.clearCookie("login"); 
-    res.redirect("/log_sign"); 
+    res.clearCookie("login");
+    res.redirect("/log_sign");
 });
 
 app.listen(PORT, () => {
